5cdfd781da0172701f547b1d27431d8fb8e073d0
kanomkway <157315857+kanomkway@users.noreply.github.com>
2024-11-03 09:36:23 +0000

    lastest

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+var mysql = require('mysql');
 const app = express()
 const port = 5000
 
@@ -19,27 +20,99 @@ app.use((req,res,next)=>{
 })
 app.use(express.json())
 
-const products=[
-  {id:0,name:"Notebook Acer Swift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
-  {id:1,name:"Notebook Asus Vivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
-  {id:2,name:"Notebook Lenovo Ideapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
-  {id:3,name:"Notebook MSI Prestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
-  {id:4,name:"Notebook DELL XPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
-  {id:5,name:"Notebook HP Envy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}
-];
+var con = mysql.createConnection({
+  host: "korawit.ddns.net",
+  user: "webapp",
+  password: "secret2024",
+  port: "3307",
+  database: "shop",
+});
+ 
+con.connect(function(err){
+  if(err) throw err;
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/products', (req, res) => {
-  if(products.length>0){
-    res.send(products)
-  } else{
-    res.status(400).send("No Product Found")
-  }
+// app.get('/api/products', (req, res) => {
+//   if(products.length>0){
+//     res.send(products)
+//   } else{
+//     res.status(400).send("No Product Found")
+//   }
+// })
+
+app.get('/api/products',(req,res)=>{
+  con.query("SELECT * FROM products",function(err,result,fields){
+    if(err) throw res.status(400).send("No products found");
+    console.log(result);
+    res.send(result);
+  });
 })
+
+app.get('/api/products/:id',(req,res)=>{
+  const id = req.params.id;
+  con.query(`SELECT * FROM products where id=${id}`,function(err,result,fields){
+    if(err) throw err;
+    if(result.length==0)
+      res.status(400).send(`No products id: ${id} found`);
+    else{
+      console.log(result);
+      res.send(result);
+    }
+  });
+})
+
+app.post('/api/addproduct',(req,res)=>{
+  const name = req.body.name
+  const price = req.body.price
+  const img = req.body.img
+  console.log(name, price, img)
+  con.query(`INSERT INTO products (name, price, img) VALUES ('${name}', '${price}', '${img}')`,function(err,result,fields){
+    if(err)
+      throw res.status(400).send(`Error. Cannot add product.`);
+    else{
+      console.log(result);
+      res.send(result);
+    }
+  });
+}
+)
+
+app.delete('/api/delproduct:id',(req,res)=>{
+  const id = req.params.id;
+  console.log(id)
+  con.query(`DELETE FROM products WHERE id=${id}`,function(err,result,fields){
+    if(err) throw res.status(400).send(`Error. Cannot delete product.`);
+    con.query(`SELECT * FROM products`,function(err,result,fields){
+      if (err) throw res.status(400).send(`Error. Cannot delete product.`)
+      console.log(result)
+      res.send({products:result,status:"ok"})
+    });
+  });
+}
+)
+
+app.delete('/api/updateproduct:id',(req,res)=>{
+  const id = req.params.id;
+  const name = req.body.name
+  const price = req.body.price
+  const img = req.body.img
+  console.log(id)
+  con.query(`UPDATE products SET name='${name}', price='${price}, img='${img} WHERE id=${id}`,function(err,result,fields){
+    if(err) throw res.status(400).send(`Error. Cannot update product.`);
+    con.query(`SELECT * FROM products`,function(err,result,fields){
+      if (err) throw res.status(400).send(`Error. Cannot update product.`)
+      console.log(result)
+      res.send({products:result,status:"ok"})
+    });
+  });
+});
+  
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
